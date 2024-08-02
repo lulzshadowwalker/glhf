9136e834df7a837f1bff324448cc2f5cd4cd2548
lulzshadowwalker <lulzshadowwalker@gmail.com>
2024-08-02 15:29:31 +0300

    Refactored shaders

--- glhf/main.cpp
+++ glhf/main.cpp
@@ -7,94 +7,12 @@
 #include "VertexBuffer.hpp"
 #include "IndexBuffer.hpp"
 #include "VertexArray.hpp"
+#include "Shader.hpp"
 
 #ifdef __APPLE__
 #include <OpenGL/gl3.h>
 #include <OpenGL/gl3ext.h>
 #endif
-
-struct ShaderProgramSource
-{
-    std::string vertexShader;
-    std::string fragmentShader;
-};
-
-static ShaderProgramSource loadShader(std::string filepath)
-{
-    std::ifstream stream(filepath);
-    
-    enum class ShaderType
-    {
-        NONE = -1, VERTEX = 0, FRAGMENT = 1,
-    };
-    
-    std::string line;
-    std::stringstream ss[2];
-    ShaderType mode = ShaderType::NONE;
-    
-    while(getline(stream, line))
-    {
-        if (line.find("#SHADER") != std::string::npos) {
-            if (line.find("VERTEX") != std::string::npos) {
-                mode = ShaderType::VERTEX;
-            } else if (line.find("FRAGMENT")) {
-                mode = ShaderType::FRAGMENT;
-            }
-        } else {
-            ASSERT((int)mode != -1);
-            ss[(int)mode] << line << "\n";
-        }
-    }
-    
-    return { ss[0].str(), ss[1].str() };
-}
-
-static unsigned int compileShader(unsigned int type, const std::string& source)
-{
-    unsigned int id = glCreateShader(type);
-    
-    const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-    
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE) {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        
-        char* message = (char*) alloca(sizeof(char) * length);
-        glGetShaderInfoLog(id, length, &length, message);
-        
-        printf("Failed to compile %s program\n", type == GL_VERTEX_SHADER ? "vertex" : "fragment");
-        printf("%s\n", message);
-        
-        glDeleteShader(id);
-        return 0;
-    }
-    
-    return id;
-}
-
-static unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
-    // A program in OpenGL is a collection of shaders that are used together for rendering.
-    unsigned int program = glCreateProgram();
-    
-    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
-    
-    GLCall(glAttachShader(program, vs));
-    GLCall(glAttachShader(program, fs));
-    GLCall(glLinkProgram(program));
-    GLCall(glValidateProgram(program));
-    
-    // Deleting the intemediate files. Kind of like deleting the intermediate .obj files in C++
-    GLCall(glDeleteShader(vs));
-    GLCall(glDeleteShader(fs));
-
-    return program;
-}
 
 int main(void)
 {
@@ -152,14 +70,11 @@ int main(void)
 
     IndexBuffer ib(indices, 6);
     
-    ShaderProgramSource shader = loadShader("res/shaders/basic.shader");
-    
-    unsigned int program = createShader(shader.vertexShader, shader.fragmentShader);
-    GLCall(glUseProgram(program));
+    Shader shader("res/shaders/basic.shader");
+    shader.Bind();
     
     // uniforms need to be defined after `glUseProgram` so they can be applied to the shader.
-    GLCall(unsigned int uColor = glGetUniformLocation(program, "u_Color"));
-    GLCall(glUniform4f(uColor, 1.0f, 0.0f, 0.0f, 1.0f));
+    shader.SetUniform4f("u_Color", 1.0f, 0.0f, 0.0f, 1.0f);
     
     float rgb[3] = {0.2f, 0.6f, 0.9f};
     float step = 0.01;
@@ -179,8 +94,7 @@ int main(void)
                               nullptr // no need to specify it here since we bound the ibo to the GL_ELEMENT_ARRAY_BUFFER slot
                               ));
         
-        
-        GLCall(glUniform4f(uColor, rgb[0], rgb[1], rgb[2], 1.0));
+         shader.SetUniform4f("u_Color", rgb[0], rgb[1], rgb[2], 1.0f);
         for (int i = 0; i < 3; i++) {
             if (rgb[i] > 1 || rgb[i] < 0) {
                 step *= -1;
@@ -204,7 +118,6 @@ int main(void)
     }
     
     // GLCall(glDisableVertexAttribArray(0)); TODO: Cleanup in either the VertexArray or IndexBuffer ?
-    GLCall(glDeleteProgram(program));
     GLCall(glfwTerminate());
     
     return 0;
---
+++ glhf/src/Shader.hpp
@@ -0,0 +1,42 @@
+//
+//  Shader.hpp
+//  glhf
+//
+//  Created by lulz on 02/08/2024.
+//
+
+#ifndef Shader_hpp
+#define Shader_hpp
+
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+
+struct ShaderProgramSource
+{
+    std::string vertexShader;
+    std::string fragmentShader;
+};
+
+class Shader
+{
+private:
+    unsigned int m_RendererID;
+    const std::string& m_Filepath;
+    std::unordered_map<std::string, int> m_UniformLocationCache;
+    
+public:
+    Shader(const std::string& m_Filepath);
+    ~Shader();
+    
+    void Bind() const;
+    void Unbind() const;
+    void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+private:
+    unsigned int CompileShader(unsigned int type, const std::string& source);
+    unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+    ShaderProgramSource LoadShader(std::string filepath);
+    unsigned int GetUniformLocation(const std::string& name) const;
+};
+
+#endif /* Shader_hpp */
